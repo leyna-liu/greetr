82a1c6a7a7cf13c8081e7192dcb015c541e9bd72
Leyna Liu <lliu170@berkeley.edu>
2025-11-19 18:48:59 -0800

    updated hello.R

--- R/hello.R
+++ R/hello.R
@@ -2,10 +2,11 @@
 #' 
 #' @param x A character vector, like a name for example
 #' @returns A character vector, "Hello [insert character vector]!"
+#' 
 #' @examples
 #' hello("Leyna")
-#' [1] "Hello Leyna !"
-#' hello <-
+
+hello <-
 function(x) {
   print(paste("Hello", x, "!"))
 }
