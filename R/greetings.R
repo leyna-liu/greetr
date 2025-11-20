8673361ad8e72001e68ab5be587d0a08532f4a24
Leyna Liu <lliu170@berkeley.edu>
2025-11-19 18:22:18 -0800

    updated greetings and hello helpfiles

---
+++ R/greetings.R
@@ -0,0 +1,13 @@
+
+#' Greetr Built In Dataset: Greetings
+#' 
+#' A  tibble of greetings in various different languages.
+#' 
+#' @format A tibble of dimensions: 5x2 (5 rows and 2 columns)
+#' \describe{
+#' \item{greeting}{A greeting in a different language stored as charactering string}
+#' \item{language}{The type of language of that specific greeting also stored as a character string}
+#' }
+#' 
+#' @source Created for STAT 133.
+"greetings"
